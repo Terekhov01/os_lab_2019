88d5e31367f859d25459a04103d3922e51f240ee
terekhov01 <lehaterekhov@mail.ru>
2021-04-07 13:38:49 +0000

    done

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,6 +1,19 @@
+#include <ctype.h>
+#include <limits.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <getopt.h>
+
+#include "utils.h"
 
 #include <pthread.h>
 
@@ -24,13 +37,6 @@ void *ThreadSum(void *args) {
   return (void *)(size_t)Sum(sum_args);
 }
 
-void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
-  srand(seed);
-  for (int i = 0; i < array_size; i++) {
-    array[i] = rand();
-  }
-}
-
 int main(int argc, char **argv) {
   /*
    *  TODO:
@@ -42,19 +48,95 @@ int main(int argc, char **argv) {
   uint32_t threads_num = 0;
   uint32_t array_size = 0;
   uint32_t seed = 0;
-  pthread_t threads[threads_num];
 
-  /*
-   * TODO:
-   * your code here
-   * Generate array here
-   */
+  while (true) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"threads_num", required_argument, 0, 0},
+                                      {"array_size", required_argument, 0, 0},
+                                      {"seed", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+
+    if (c == -1) break;
+
+    switch (c) {
+      case 0:
+        switch (option_index) {
+          case 0:
+            threads_num = atoi(optarg);
+            // your code here
+            // error handling
+            if (threads_num <= 0)
+            {
+                printf("threads_num must be positive");
+                seed = -1;
+            }
+            break;
+          case 1:
+            array_size = atoi(optarg);
+            // your code here
+            // error handling
+            if (array_size <= 0)
+            {
+                printf("arraysize must be positive");
+                array_size = -1;
+            }
+            break;
+          case 2:
+            seed = atoi(optarg);
+            if (seed <= 0)
+            {
+                printf("seed is positive number");
+                seed = -1;
+            }
+            break; 
+
+          defalut:
+            printf("Index %d is out of options\n", option_index);
+        }
+        break;
+      case '?':
+        break;
+
+      default:
+        printf("getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (optind < argc) {
+    printf("Has at least one no option argument\n");
+    return 1;
+  }
+  if (seed == -1 || array_size == -1 || threads_num == -1) {
+    printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" --timeout \"timeout\"\n",
+           argv[0]);
+    return 1;
+  }
+  pthread_t threads[threads_num];
   int *array = malloc(sizeof(int) * array_size);
   GenerateArray(array, array_size, seed);
 
+  int result = 0;
+  for(int i=0;i<array_size;i++)
+  {
+      result += array[i];
+      printf("%d | %d\n", array[i], result);
+  }
+  printf("vot eto nujno poluchit': %d\n", result);
+  printf("\n");
+
+  struct timeval start_time;
+  gettimeofday(&start_time, NULL);
+
+  int n = array_size/threads_num;
+
   struct SumArgs args[threads_num];
   for (uint32_t i = 0; i < threads_num; i++) {
-    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
+    args[i].array = array; args[i].begin = i * n; args[i].end = (i+1)*n;
+    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args[i])) {
       printf("Error: pthread_create failed!\n");
       return 1;
     }
